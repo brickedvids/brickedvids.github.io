71dd38e6f7907fbcb5fbf0839963f7ee6ec57590
brickedvids <info@brickedvids.com>
2024-07-11 21:03:11 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,6 @@ import remarkGfm from 'remark-gfm'
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
   output: "export",
-  basePath: "/brickedvids",
   images: {
     unoptimized: true,
   },
